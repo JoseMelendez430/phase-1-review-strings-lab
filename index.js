607f1c90cddb80ca0c90c79072bf26a7490a12dc
Jose <joseamelendez1301@gmail.com>
2023-11-17 11:40:55 -0500

    completed js strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Jose Melendez'
+const welcomeMessage = 'Welcome to Flatbook, Jose Melendez!'
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
